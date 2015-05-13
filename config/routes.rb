6c0c2c8b19cbc63dbeccff7045763fcf0ec215ea
Ekechi Ikenna <luccy559slim@gmail.com>
2015-05-13 23:24:55 +0100

    users, devise views styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'products#index'
   resources :invoices
-
   resources :releases
-
   resources :consignments
-
   resources :products
+  devise_for :users, skip: [:registrations]
 
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
