e1ec802abdbbd8424444239461c2dcd06b9d8fdd
Adefila Sunday <sunday.adefila@andela.co>
2015-06-24 08:33:46 +0100

    validates releasing a purchase only once

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,6 +1,19 @@
 class Release < ActiveRecord::Base
   belongs_to :user
   belongs_to :purchase
+  before_create :check_if_collected
+  after_create :mark_as_collected
 
   validates_presence_of :confirmation_code, :user, :purchase
+  #todo validates uniqueness of a purchase how?
+
+  private
+
+    def check_if_collected
+      self.purchase.collected == false
+    end
+
+    def mark_as_collected
+      self.purchase.update_attributes!(collected: true)
+    end
 end
